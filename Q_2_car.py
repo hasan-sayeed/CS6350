0237eb60c0620c6bdf92f3aa1739e32f7aa6d213
Hasan Sayeed <hasan.sayeed.71.93@gmail.com>
2022-09-22 23:42:27 -0600

    6th commit

---
+++ Q_2_car.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from id3_functions import id3, predict
+
+df_result_train = pd.DataFrame(columns = ['Information Gain', 'Majority Error', 'Gini Index'],
+                                index = ['1', '2', '3', '4', '5', '6'])
+df_result_test = pd.DataFrame(columns = ['Information Gain', 'Majority Error', 'Gini Index'],
+                                index = ['1', '2', '3', '4', '5', '6'])
+
+df_train = pd.read_csv('data/car/train.csv')
+df_test = pd.read_csv('data/car/test.csv')
+
+metrics = ['entropy', 'majority error', 'gini index']
+t_ds = [1, 2, 3, 4, 5, 6]
+
+dict_train = {'entropy': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None},
+         'majority error': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None},
+         'gini index': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None}}
+
+dict_test = {'entropy': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None},
+         'majority error': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None},
+         'gini index': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None}}
+
+for metric in metrics:
+    for t_d in t_ds:
+
+        # train
+        tree= id3(df_train, metric = metric, tree_depth = t_d)
+
+        #  Accuracy
+        train_accuracy = predict(df_train, tree)
+        test_accuracy = predict(df_test, tree)
+        # print(train_accuracy, test_accuracy)
+        dict_train[metric][t_d] = train_accuracy
+        dict_test[metric][t_d] = test_accuracy
+        print(t_d, 'training done!')
+
+
+print('Accuracy in training set for 3 different heuristics. Index value indicates tree depth.\n', pd.DataFrame.from_dict(dict_train))
+print('Accuracy in test set for 3 different heuristics. Index value indicates tree depth.\n', pd.DataFrame.from_dict(dict_test))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# b = id3(df_train, metric = 'majority error', tree_depth = 1)
+# print(b)
+
+# Y_label = []
+# for i in range(len(df_train)):
+#   inst = df_train.iloc[i,:]
+#   prediction = predict_core(inst, tree)
+#   Y_label.append(prediction)
+# # print(Y_label)
+
+# def vals(x):
+#     if isinstance(x, dict):
+#         result = []
+#         for v in x.values():
+#             result.extend(vals(v))
+#         return result
+#     else:
+#         return [x]
+
+# d = vals(tree)
+# print(d)
--- Q_3_bank.py
+++ Q_3_bank.py
@@ -1,16 +1,16 @@
 import pandas as pd
-from functions import id3, predict
+from id3_functions import id3, predict
 
 df_result_train = pd.DataFrame(columns = ['Information Gain', 'Majority Error', 'Gini Index'],
                                 index = ['1', '2', '3', '4', '5', '6'])
 df_result_test = pd.DataFrame(columns = ['Information Gain', 'Majority Error', 'Gini Index'],
                                 index = ['1', '2', '3', '4', '5', '6'])
 
-df_train = pd.read_csv('data/car/train.csv')
-df_test = pd.read_csv('data/car/test.csv')
+df_train = pd.read_csv('data/bank/train.csv')
+df_test = pd.read_csv('data/bank/test.csv')
 
 metrics = ['entropy', 'majority error', 'gini index']
-t_ds = [1, 2, 3, 4, 5, 6]
+t_ds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 dict_train = {'entropy': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None},
          'majority error': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None},
@@ -24,7 +24,7 @@ for metric in metrics:
     for t_d in t_ds:
 
         # train
-        tree= id3(df_train, metric = 'majority error', tree_depth = t_d)
+        tree= id3(df_train, metric = metric, tree_depth = t_d)
 
         #  Accuracy
         train_accuracy = predict(df_train, tree)
@@ -32,55 +32,8 @@ for metric in metrics:
         # print(train_accuracy, test_accuracy)
         dict_train[metric][t_d] = train_accuracy
         dict_test[metric][t_d] = test_accuracy
+        print(t_d, 'training done!')
 
 
 print('Accuracy in training set for 3 different heuristics. Index value indicates tree depth.\n', pd.DataFrame.from_dict(dict_train))
 print('Accuracy in test set for 3 different heuristics. Index value indicates tree depth.\n', pd.DataFrame.from_dict(dict_test))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# b = id3(df_train, metric = 'majority error', tree_depth = 1)
-# print(b)
-
-# Y_label = []
-# for i in range(len(df_train)):
-#   inst = df_train.iloc[i,:]
-#   prediction = predict_core(inst, tree)
-#   Y_label.append(prediction)
-# # print(Y_label)
-
-# def vals(x):
-#     if isinstance(x, dict):
-#         result = []
-#         for v in x.values():
-#             result.extend(vals(v))
-#         return result
-#     else:
-#         return [x]
-
-# d = vals(tree)
-# print(d)
