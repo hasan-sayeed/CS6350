70953c3a51caf5da39b754de0bac03110051a7de
Hasan Sayeed <hasan.sayeed.71.93@gmail.com>
2022-09-22 23:19:54 -0600

    5th commit

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 import numpy as np
+from statistics import mode
+from sklearn.metrics import accuracy_score
+from sklearn import metrics
 
 # Entropy of the dataset
 
@@ -11,7 +14,7 @@ def entropy_of_total(df):
     total_entropy += -probability * np.log2(probability)
   return np.float64(total_entropy)
 
-# Entropy of attribute
+# Entropy of attributes
 
 def entropy_of_attribute(df, attribute):
   label = df.keys()[-1]
@@ -28,13 +31,87 @@ def entropy_of_attribute(df, attribute):
     entropy += (den/len(df))*entropy_tmp
   return np.float64(entropy)
 
+# Majority error of the dataset
+
+def me_of_total(df):
+  label = df.keys()[-1]
+  label_vals, label_counts = np.unique(df[label], return_counts = True)   # unique labels
+  majority_value_count = np.amax(label_counts)
+  probability = majority_value_count/len(df[label])
+  # for label_val in label_vals:
+  #   probability = df[label].value_counts()[label_val]/len(df[label])
+  #   total_me += -probability * np.log2(probability)
+  total_me = 1 - probability
+  return np.float64(total_me)
+
+# Majority error of attributes
+
+def me_of_attribute(df, attribute):
+  label = df.keys()[-1]
+  attr_vals = df[attribute].unique()
+  # label_vals, label_counts = np.unique(df[label][df[attribute] == attr_val], return_counts = True)
+  me = 0
+  for attr_val in attr_vals:
+    attr_label_vals, attr_label_counts = np.unique(df[label][df[attribute] == attr_val], return_counts = True)
+    majority_value_count = np.amax(attr_label_counts)
+    # num = len(df[attribute][df[attribute] == attr_val][df[label] == label_val])
+    den = len(df[attribute][df[attribute] == attr_val])
+    probability = majority_value_count/den
+    me_tmp = 1 - probability
+    # for label_val in attr_label_vals:
+    #   num = len(df[attribute][df[attribute] == attr_val][df[label] == label_val])
+    #   den = len(df[attribute][df[attribute] == attr_val])
+    #   probability = num/den
+    #   me_tmp += -probability * np.log2(probability + 0.000001)
+    me += (den/len(df))*me_tmp
+  return np.float64(me)
+
+# Gini index of the dataset
+
+def gi_of_total(df):
+  label = df.keys()[-1]
+  counts = df[label].unique()    # unique labels
+  total_gi = 1
+  for count in counts:
+    probability = df[label].value_counts()[count]/len(df[label])
+    total_gi += -probability**2
+  return np.float64(total_gi)
+
+# Gini index of attributes
+
+def gi_of_attribute(df, attribute):
+  label = df.keys()[-1]
+  attr_vals = df[attribute].unique()
+  target_vals = df[label].unique()
+  gi = 0
+  for attr_val in attr_vals:
+    gi_tmp = 1
+    for target_val in target_vals:
+      num = len(df[attribute][df[attribute] == attr_val][df[label] == target_val])
+      den = len(df[attribute][df[attribute] == attr_val])
+      probability = num/den
+      gi_tmp += -probability**2
+    gi += (den/len(df))*gi_tmp
+  return np.float64(gi)
+
 # Information gain calculation and best attribute
 
-def best_attribute(df):
+def best_attribute(df, metric):
   IG = []
-  for key in df.keys()[:-1]:
-    IG.append(entropy_of_total(df) - entropy_of_attribute(df, key))
-  return df.keys()[:-1][np.argmax(IG)]
+  if metric=='entropy':
+    for key in df.keys()[:-1]:
+      IG.append(entropy_of_total(df) - entropy_of_attribute(df, key))
+    return df.keys()[:-1][np.argmax(IG)]
+  
+  elif metric=='majority error':
+    for key in df.keys()[:-1]:
+      IG.append(me_of_total(df) - me_of_attribute(df, key))
+    return df.keys()[:-1][np.argmax(IG)]
+
+  elif metric=='gini index':
+    for key in df.keys()[:-1]:
+      IG.append(gi_of_total(df) - gi_of_attribute(df, key))
+    return df.keys()[:-1][np.argmax(IG)]
 
 # Updated dataframe
 
@@ -43,10 +120,22 @@ def updated_dataframe(df, attribute, val):
 
 # construct the tree
 
-def construct_tree(df, tree = None):
-  node = best_attribute(df)               # Get the best attribute first
+def id3(df, tree = None, metric = 'entropy', tree_depth= 1000):
+  
+  '''
+  Parameters
+        ----------
+        metric : str, optional
+            Which varient of information gain you want to use. Possible values are
+            "entropy", "majority error" and "gini index", by default "entropy.
+        tree_depth : str, optional
+            Maximum tree depth you want, by default 1000.
+  '''
+
+  node = best_attribute(df, metric)               # Get the best attribute first
   att_counts = np.unique(df[node])
   label = df.keys()[-1]
+  t_d = 0
   if tree is None:
     tree = {}
     tree[node] = {}
@@ -56,16 +145,62 @@ def construct_tree(df, tree = None):
     if len(label_counts) == 1:
       tree[node][att_count] = label_vals[0]
     else:
-      tree[node][att_count] = construct_tree(updated_data)
-  return tree
+      t_d += 1
+      # tree[node][att_count] = construct_tree(updated_data)
+      if t_d<tree_depth:
+        tree[node][att_count] = id3(updated_data)
+      elif t_d==tree_depth:
+        majority_label = np.where(label_counts == np.amax(label_counts))
+        tree[node][att_count] = label_vals[majority_label[0][0]]
+      
+  return tree#, metric, tree_depth
 
-def predict(inst, tree):
-  for node in tree.keys():
-    value = inst[node]
-    tree = tree[node][value]
-    prediction = 0
-    if type(tree) is dict:
-      prediction = predict(inst, tree)
+def vals(x):
+    if isinstance(x, dict):
+        result = []
+        for v in x.values():
+            result.extend(vals(v))
+        return result
     else:
-      prediction = tree
+        return [x]
+
+def predict_core(inst, tree):
+  for node in tree.keys():
+    prediction = 0
+    value = inst[node]
+    try:
+      tree = tree[node][value]
+      if type(tree) is dict:
+        prediction = predict_core(inst, tree)
+      else:
+        prediction = tree
+    except KeyError:
+        prediction = mode(vals(tree))
+       
   return prediction
+
+def predict(df, tree):
+  y_predict = []
+  for i in range(len(df)):
+    inst = df.iloc[i,:]
+    prediction = predict_core(inst, tree)
+    y_predict.append(prediction)
+  accuracy = metrics.accuracy_score(df[df.columns[-1]], y_predict)
+  return accuracy
+
+# def id3(df, metric = 'entropy', tree_depth = 1000):
+  
+#   '''
+#   Parameters
+#         ----------
+#         metric : str, optional
+#             Which varient of information gain you want to use. Possible values are
+#             "entropy", "majority error" and "gini index", by default "entropy.
+#         tree_depth : str, optional
+#             Maximum tree depth you want, by default 1000.
+#   '''
+
+#   tree = construct_tree(df, metric, tree_depth)
+
+  
+#   return tree
--- id3.py
+++ id3.py
@@ -1,8 +1,86 @@
 import pandas as pd
-from functions import construct_tree
+from functions import id3, predict
 
+df_result_train = pd.DataFrame(columns = ['Information Gain', 'Majority Error', 'Gini Index'],
+                                index = ['1', '2', '3', '4', '5', '6'])
+df_result_test = pd.DataFrame(columns = ['Information Gain', 'Majority Error', 'Gini Index'],
+                                index = ['1', '2', '3', '4', '5', '6'])
 
 df_train = pd.read_csv('data/car/train.csv')
-tree = construct_tree(df_train)
-import pprint
-pprint.pprint(tree)
+df_test = pd.read_csv('data/car/test.csv')
+
+metrics = ['entropy', 'majority error', 'gini index']
+t_ds = [1, 2, 3, 4, 5, 6]
+
+dict_train = {'entropy': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None},
+         'majority error': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None},
+         'gini index': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None}}
+
+dict_test = {'entropy': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None},
+         'majority error': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None},
+         'gini index': {1: None, 2: None, 3: None, 4: None, 5: None, 6: None}}
+
+for metric in metrics:
+    for t_d in t_ds:
+
+        # train
+        tree= id3(df_train, metric = 'majority error', tree_depth = t_d)
+
+        #  Accuracy
+        train_accuracy = predict(df_train, tree)
+        test_accuracy = predict(df_test, tree)
+        # print(train_accuracy, test_accuracy)
+        dict_train[metric][t_d] = train_accuracy
+        dict_test[metric][t_d] = test_accuracy
+
+
+print('Accuracy in training set for 3 different heuristics. Index value indicates tree depth.\n', pd.DataFrame.from_dict(dict_train))
+print('Accuracy in test set for 3 different heuristics. Index value indicates tree depth.\n', pd.DataFrame.from_dict(dict_test))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# b = id3(df_train, metric = 'majority error', tree_depth = 1)
+# print(b)
+
+# Y_label = []
+# for i in range(len(df_train)):
+#   inst = df_train.iloc[i,:]
+#   prediction = predict_core(inst, tree)
+#   Y_label.append(prediction)
+# # print(Y_label)
+
+# def vals(x):
+#     if isinstance(x, dict):
+#         result = []
+#         for v in x.values():
+#             result.extend(vals(v))
+#         return result
+#     else:
+#         return [x]
+
+# d = vals(tree)
+# print(d)
